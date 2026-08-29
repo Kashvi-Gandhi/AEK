13e20e76923173e6da2b0e1d21ce5bf897f83895
unknown <kashvig2014@gmail.com>
2026-08-29 13:19:47 +0530

    trying to implement user details

--- src/client/user-details.js
+++ src/client/user-details.js
@@ -2,6 +2,6 @@ import React from "react";
 import "react-dom";
 import {reactRender} from "@ombiel/aek-lib";
 
-import Screen from "./user-details/screen";
+import Screen from "../client/user-details/screen";
 
 reactRender(<Screen />);
--- src/client/user-details/screen.js
+++ src/client/user-details/screen.js
@@ -1,33 +1,63 @@
 import React from "react";
-import {
-  Container,
-  VBox,
-  BannerHeader,
-  BasicSegment,
-} from "@ombiel/aek-lib";
-
+import { Container, VBox, BannerHeader, BasicSegment } from "@ombiel/aek-lib";
 
 export default class Screen extends React.Component {
-
-  componentDidMount() {
-
-  }
-
   render() {
+    // Retrieves user details from window object (supports both direct object or nested details object)
+    const rawData = window.userDetails || {};
+    const userDetails = rawData.details || rawData;
+
+    const contactId = userDetails.ContactID || userDetails.username || "Not available";
+    const firstName = userDetails.FirstName || userDetails.firstName || userDetails.givenName || "Not available";
+    const lastName = userDetails.LastName || userDetails.lastName || userDetails.surname || "Not available";
+    const email = userDetails.EmailAddress || userDetails.emailAddress || userDetails.email || "Not available";
+
+    const hasAttributes = Object.keys(userDetails).length > 0;
 
     return (
       <Container>
         <VBox>
-          <BannerHeader theme="alt" key="header" data-flex={0}>User Details</BannerHeader>
+          <BannerHeader theme="alt" key="header" data-flex={0}>
+            User Details
+          </BannerHeader>
+
           <BasicSegment>
-            <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
-            <p>Sed posuere consectetur est at lobortis.</p>
+            <h3>My Details</h3>
+
+            <p>
+              <strong>Contact ID:</strong> {contactId}
+            </p>
+
+            <p>
+              <strong>First Name:</strong> {firstName}
+            </p>
+
+            <p>
+              <strong>Last Name:</strong> {lastName}
+            </p>
+
+            <p>
+              <strong>Email Address:</strong> {email}
+            </p>
+
+            <hr />
+
+            <h4>All Available User Attributes</h4>
+
+            <pre
+              style={{
+                overflowX: "auto",
+                padding: "10px",
+                background: "#f5f5f5",
+              }}
+            >
+              {hasAttributes
+                ? JSON.stringify(userDetails, null, 2)
+                : "Not available"}
+            </pre>
           </BasicSegment>
         </VBox>
       </Container>
     );
-
   }
-
 }
