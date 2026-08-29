d57373f2dc5229ee3ec8c5efaa018da0a845beed
unknown <kashvig2014@gmail.com>
2026-08-29 11:30:06 +0530

    solving the screen-2 deployment try-3

--- src/client/screen.js
+++ src/client/screen.js
@@ -2,6 +2,6 @@ import React from "react";
 import "react-dom";
 import {reactRender} from "@ombiel/aek-lib";
 
-import Screen from "../client/screen-2/screen";
+import Screen from "../client/screen/screen";
 
 reactRender(<Screen />);
